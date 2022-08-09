145545bf8e804693bc1f935772e296bd7e54c68f
DESKTOP-H27OSM9\hp <satheesh18.97@gmail.com>
2022-08-09 11:55:45 +0530

    modified code

--- Demoproject/src/test/java/Samplecode.java
+++ Demoproject/src/test/java/Samplecode.java
@@ -2,8 +2,8 @@
 public class Samplecode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("satheesh");
+		System.out.println("deadpool");
 	}
 
 }
